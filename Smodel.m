e2c8efb53288f346095a37ec2f375fecad211813
Ran Tao <04small@gmail.com>
2017-01-06 16:04:31 +0800

    Update Smodel.m
    多步控制，施加一步

--- Smodel.m
+++ Smodel.m
@@ -9,7 +9,11 @@ trafficlight = green_linear;
 x = green_linear(1,:)';
 lb = 15*ones(nodes_num,1);
 ub = 45*ones(nodes_num,1);
+x = [x;x;x];
+lb = [lb;lb;lb];
+ub = [ub;ub;ub];
 res = fmincon(@(x)objective(x,freesp,wait),x,[],[],[],[],lb,ub);
+res = res(1:11);
 
 trafficlight(1:2, :) = [res'; (60.-res)'];
 
@@ -41,12 +45,16 @@ function res = objective(greenlight,freesp,wait)
     end
     % 生成转弯矩阵beta ,greenlight矩阵(右转为60),排队长度矩阵queue
     queue = zeros(29,29);
-    green = zeros(29,29);
+    green_1 = zeros(29,29);
+    green_2 = zeros(29,29);
+    green_3 = zeros(29,29);
     miu = zeros(29,29);
     for i = 1:28
         link_from = i;
         lane_num = linkData(i,4);
-        greentime = greenlight(linkData(i,2)+(1-linkData(i,5))*11);
+        greentime_1 = greenlight(linkData(i,2)+(1-linkData(i,5))*11*3);
+        greentime_2 = greenlight(11+linkData(i,2)+(1-linkData(i,5))*11*3);
+        greentime_3 = greenlight(22+linkData(i,2)+(1-linkData(i,5))*11*3);
         if linkData(i,7)>0 % 左转
             if linkData(i,7)>11
                 link_to = 29;
@@ -55,7 +63,9 @@ function res = objective(greenlight,freesp,wait)
                 link_to = transform(linkData(i,2),linkData(i,7));
             end        
             miu(link_from,link_to)=1/lane_num;
-            green(link_from,link_to)=greentime;
+            green_1(link_from,link_to)=greentime_1;
+            green_2(link_from,link_to)=greentime_2;
+            green_3(link_from,link_to)=greentime_3;
             queue(link_from,link_to)=waiting(link_from,1);
         end
         if linkData(i,8)>0 % 直行
@@ -66,7 +76,9 @@ function res = objective(greenlight,freesp,wait)
                 link_to = transform(linkData(i,2),linkData(i,8));
             end
             miu(link_from,link_to)=1/lane_num;
-            green(link_from,link_to)=greentime;
+            green_1(link_from,link_to)=greentime_1;
+            green_2(link_from,link_to)=greentime_2;
+            green_3(link_from,link_to)=greentime_3;
             queue(link_from,link_to)=waiting(link_from,2);
         end
         if linkData(i,9)>0 % 右转
@@ -77,7 +89,9 @@ function res = objective(greenlight,freesp,wait)
                 link_to = transform(linkData(i,2),linkData(i,9));
             end
             miu(link_from,link_to)=1/lane_num;
-            green(link_from,link_to)=60;
+            green_1(link_from,link_to)=60;
+            green_2(link_from,link_to)=60;
+            green_3(link_from,link_to)=60;
             queue(link_from,link_to)=waiting(link_from,3);
         end
     end
@@ -93,7 +107,7 @@ function res = objective(greenlight,freesp,wait)
                     a = miu(i,j)*30*kk;
                 else
                     b = queue(i,j);
-                    a = miu(i,j)*green(i,j)*kk;
+                    a = miu(i,j)*green_1(i,j)*kk;
                 end
                 c = miu(i,j)*freespace(j);
                 leavemat(i,j) = min(min(a,b),c);
@@ -126,7 +140,7 @@ function res = objective(greenlight,freesp,wait)
                     a = miu(i,j)*30*kk;
                 else
                     b = queue(i,j);
-                    a = miu(i,j)*green(i,j)*kk;
+                    a = miu(i,j)*green_2(i,j)*kk;
                 end
                 c = miu(i,j)*freespace(j);
                 leavemat(i,j) = min(min(a,b),c);
@@ -159,7 +173,7 @@ function res = objective(greenlight,freesp,wait)
                     a = miu(i,j)*30*kk;
                 else
                     b = queue(i,j);
-                    a = miu(i,j)*green(i,j)*kk;
+                    a = miu(i,j)*green_3(i,j)*kk;
                 end
                 c = miu(i,j)*freespace(j);
                 leavemat(i,j) = min(min(a,b),c);
