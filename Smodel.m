8a6818395e05f1fabf900e841e2de2c8c1a837b4
Ran Tao <04small@gmail.com>
2017-01-07 20:45:12 +0800

    Update Smodel.m
    增加了对n的约束 输出车辆数

--- Smodel.m
+++ Smodel.m
@@ -18,7 +18,7 @@ ub = 45*ones(nodes_num,1);
 x = [x;x;x];
 lb = [lb;lb;lb];
 ub = [ub;ub;ub];
-res = fmincon(@(x)objective(x,freesp,wait),x,[],[],[],[],lb,ub);
+res = fmincon(@(x)objective(x,freesp,wait),x,[],[],[],[],lb,ub,@(x) ceq(x,freesp,wait));
 % problem = createOptimProblem('fmincon','objective',@(x)objective(x,freesp,wait),'x0',x,'lb',lb,'ub',ub);
 % gs = GlobalSearch('TolX',1,'MaxTime',120);
 % [res,f] = run(gs,problem);
@@ -34,6 +34,7 @@ light_res = [light_res,res];
 freesp_res = [freesp_res,freesp];
 wait_res = [wait_res,wait];
 content_res = [content_res, n./capacity];
+TTS_res = [TTS_res,sum(n)];
 save('D:\data_lyy\sNfMedium\data_res','content_res','-append');
 save('D:\data_lyy\sNfMedium\data_res','wait_res','freesp_res','light_res','time_res','-append');
 end
@@ -212,5 +213,182 @@ function res = objective(greenlight,freesp,wait)
     % 更新车辆数
     n_3_forecast = n_2_forecast + n_enter(1:28) - n_leave(1:28);
     res = sum(n_1_forecast+n_2_forecast+n_3_forecast);
+
+end
+
+function [g,h] = ceq(greenlight,freesp,wait)
+
+    greenlight = [greenlight;60.-greenlight];
+    % greenlight = 30 * ones(22, 1);
+    % load data;
+
+    load F:\taoran\TTS\freespaceMat;
+    load F:\taoran\TTS\linkData;
+    load F:\taoran\TTS\inputLinkData;
+    load F:\taoran\TTS\capacity;
+    load F:\taoran\TTS\turnIn;
+
+    freespace = freespaceMat * freesp; % 将读出的数据转化为与linkData相对应的freesp
+    waiting = freespaceMat * wait;
+    kk = 1; %车辆通过速率
+    n = capacity - freespace;
+    % 增加源节点信息
+    freespace = [freespace;9999];
+    % 路段路口对应矩阵
+    transform = zeros(11,11);
+    for i = 1:28
+       transform(linkData(i,1),linkData(i,2))=i; 
+    end
+    % 生成转弯矩阵beta ,greenlight矩阵(右转为60),排队长度矩阵queue
+    queue = zeros(29,29);
+    green_1 = zeros(29,29);
+    green_2 = zeros(29,29);
+    green_3 = zeros(29,29);
+    miu = zeros(29,29);
+    for i = 1:28
+        link_from = i;
+        lane_num = linkData(i,4);
+        greentime_1 = greenlight(linkData(i,2)+(1-linkData(i,5))*11*3);
+        greentime_2 = greenlight(11+linkData(i,2)+(1-linkData(i,5))*11*3);
+        greentime_3 = greenlight(22+linkData(i,2)+(1-linkData(i,5))*11*3);
+        if linkData(i,7)>0 % 左转
+            if linkData(i,7)>11
+                link_to = 29;
+                miu(link_to,link_from) = 1/lane_num;
+            else
+                link_to = transform(linkData(i,2),linkData(i,7));
+            end        
+            miu(link_from,link_to)=1/lane_num;
+            green_1(link_from,link_to)=greentime_1;
+            green_2(link_from,link_to)=greentime_2;
+            green_3(link_from,link_to)=greentime_3;
+            queue(link_from,link_to)=waiting(link_from,1);
+        end
+        if linkData(i,8)>0 % 直行
+            if linkData(i,8)>11
+                link_to = 29;
+                miu(link_to,link_from) = 1/lane_num;
+            else
+                link_to = transform(linkData(i,2),linkData(i,8));
+            end
+            miu(link_from,link_to)=1/lane_num;
+            green_1(link_from,link_to)=greentime_1;
+            green_2(link_from,link_to)=greentime_2;
+            green_3(link_from,link_to)=greentime_3;
+            queue(link_from,link_to)=waiting(link_from,2);
+        end
+        if linkData(i,9)>0 % 右转
+            if linkData(i,9)>11
+                link_to = 29;
+                miu(link_to,link_from) = 1/lane_num;
+            else
+                link_to = transform(linkData(i,2),linkData(i,9));
+            end
+            miu(link_from,link_to)=1/lane_num;
+            green_1(link_from,link_to)=60;
+            green_2(link_from,link_to)=60;
+            green_3(link_from,link_to)=60;
+            queue(link_from,link_to)=waiting(link_from,3);
+        end
+    end
+    % 计算下一时刻离开车辆数
+    leavemat = zeros(29,29);
+    n_leave = zeros(29,1);
+    n_enter = zeros(29,1);
+    for i = 1:29
+        for j = 1:29
+            if miu(i,j)>0
+                if(i>28)
+                    b = 9999;
+                    a = miu(i,j)*30*kk;
+                else
+                    b = queue(i,j);
+                    a = miu(i,j)*green_1(i,j)*kk;
+                end
+                c = miu(i,j)*freespace(j);
+                leavemat(i,j) = min(min(a,b),c);
+                n_leave(i) = n_leave(i)+leavemat(i,j);
+                n_enter(j) = n_enter(j)+leavemat(i,j);
+            end
+        end
+    end
+    % 更新queue矩阵
+    for i = 1:28
+        for j = 1:29
+            if miu(i,j)>0
+                queue(i,j)=queue(i,j)+n_enter(i)*miu(i,j)-leavemat(i,j);
+            end
+        end
+    end
+    % 更新车辆数
+    n_1_forecast = n + n_enter(1:28) - n_leave(1:28);
+    freespace = [capacity-n_1_forecast;9999];
+
+    % 计算下一时刻离开车辆数
+    leavemat = zeros(29,29);
+    n_leave = zeros(29,1);
+    n_enter = zeros(29,1);
+    for i = 1:29
+        for j = 1:29
+            if miu(i,j)>0
+                if(i>28)
+                    b = 9999;
+                    a = miu(i,j)*30*kk;
+                else
+                    b = queue(i,j);
+                    a = miu(i,j)*green_2(i,j)*kk;
+                end
+                c = miu(i,j)*freespace(j);
+                leavemat(i,j) = min(min(a,b),c);
+                n_leave(i) = n_leave(i)+leavemat(i,j);
+                n_enter(j) = n_enter(j)+leavemat(i,j);
+            end
+        end
+    end
+    % 更新queue矩阵
+    for i = 1:28
+        for j = 1:29
+            if miu(i,j)>0
+                queue(i,j)=queue(i,j)+n_enter(i)*miu(i,j)-leavemat(i,j);
+            end
+        end
+    end
+    % 更新车辆数
+    n_2_forecast = n_1_forecast + n_enter(1:28) - n_leave(1:28);
+    freespace = [capacity-n_2_forecast;9999];
+
+    
+    leavemat = zeros(29,29);
+    n_leave = zeros(29,1);
+    n_enter = zeros(29,1);
+    for i = 1:29
+        for j = 1:29
+            if miu(i,j)>0
+                if(i>28)
+                    b = 9999;
+                    a = miu(i,j)*30*kk;
+                else
+                    b = queue(i,j);
+                    a = miu(i,j)*green_3(i,j)*kk;
+                end
+                c = miu(i,j)*freespace(j);
+                leavemat(i,j) = min(min(a,b),c);
+                n_leave(i) = n_leave(i)+leavemat(i,j);
+                n_enter(j) = n_enter(j)+leavemat(i,j);
+            end
+        end
+    end
+    % 更新queue矩阵
+    for i = 1:28
+        for j = 1:29
+            if miu(i,j)>0
+                queue(i,j)=queue(i,j)+n_enter(i)*miu(i,j)-leavemat(i,j);
+            end
+        end
+    end
+    % 更新车辆数
+    n_3_forecast = n_2_forecast + n_enter(1:28) - n_leave(1:28);
+    g = [n_1_forecast-capacity;n_2_forecast-capacity;n_3_forecast-capacity];
+    h = [];
 
 end
